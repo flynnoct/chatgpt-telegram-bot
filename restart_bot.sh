1b6018cace209f570801dee2b6731e434482b964
Flynn <me@flynmail.com>
2023-03-04 06:34:16 +0000

    Implemented start/stop/restart bash script

--- restart_bot.sh
+++ restart_bot.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# This script restarts a Python program named telegram_message_parser.py
 
-ps -ef | grep telegram_message_parser.py | grep -v grep | awk '{pid=$2}' | xargs kill -15
+echo "Restarting bot..."
 
-./start_bot.sh
+pid=`ps -ef | grep telegram_message_parser.py | grep -v grep | awk '{print $2}'`
 
+if [ -z $pid ]; then
+    echo "Bot is not running"
+else
+    echo "Bot has been terminated"
+    kill -9 $pid
+fi
+
+echo "Starting bot..."
+nohup python3 telegram_message_parser.py >/dev/null 2>&1 &
+echo "Bot has been restarted successfully"
--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# This script start a Python program named telegram_message_parser.py and run it in the background
 
-nohup python3 telegram_message_parser.py &
+nohup python3 telegram_message_parser.py >/dev/null 2>&1 &
+echo "Bot has been started"
