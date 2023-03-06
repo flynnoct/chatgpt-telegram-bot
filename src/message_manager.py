11fcd205b67d04bb5a8ceedd5cd2c8d9e12b37c8
RayCxggg <chexj18@163.com>
2023-03-06 22:06:33 +0800

    add num para to check_image_generation_allowed

--- src/message_manager.py
+++ src/message_manager.py
@@ -6,13 +6,14 @@ from access_manager import AccessManager
 from chat_session import ChatSession
 from openai_parser import OpenAIParser
 
+
 class MessageManager:
-    
+
     userDict = {}
     config_dict = {}
     openai_parser = None
     access_manager = None
-    
+
     def __init__(self):
         self.openai_parser = OpenAIParser()
         self.access_manager = AccessManager()
@@ -20,33 +21,33 @@ class MessageManager:
         with open("config.json") as f:
             self.config_dict = json.load(f)
 
-    
     def get_response(self, id, user, message):
 
         t = time.time()
-        
+
         (permission, clue) = self.access_manager.check_user_allowed(user)
         if permission == False:
             return clue
-          
+
         if id not in self.userDict:
             # new user
             self.userDict[id] = ChatSession(t, message)
         else:
             self.userDict[id].update(t, message, "user")
-           
-        # send user info for statistics 
-        (answer, usage) = self.__sendMessage(user, self.userDict[id].messageList)
+
+        # send user info for statistics
+        (answer, usage) = self.__sendMessage(
+            user, self.userDict[id].messageList)
         self.userDict[id].update(t, answer, "assistant")
         self.access_manager.update_usage_info(user, usage, "chat")
         return answer
-    
+
     def clear_context(self, id):
         try:
             self.userDict[id].clear_context(time.time())
         except Exception as e:
             print(e)
-            
+
     def get_generated_image_url(self, user, prompt):
 
         # Temporary fix by @Flynn, will be fixed in the next version
@@ -62,22 +63,21 @@ class MessageManager:
             return clue
 
         (url, usage) = self.openai_parser.image_generation(user, prompt)
-        
+
         self.access_manager.update_usage_info(user, usage, "image")
         return (url, clue)
-            
+
     def get_transcript(self, user, audio_file):
         (permission, clue) = self.access_manager.check_user_allowed(user)
         if permission == False:
             return clue
-        
+
         try:
             return self.openai_parser.speech_to_text(user, audio_file)
         except Exception as e:
             print(e)
             return ""
-        
-           
+
     def __sendMessage(self, user, messageList):
         ans = self.openai_parser.get_response(user, messageList)
         return ans
