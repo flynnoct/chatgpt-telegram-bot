adf68a8359189dfb69b85ba4346700dc9dee8ad9
Flynn <me@flynmail.com>
2023-03-06 08:14:24 +0000

    Split update usage funcs from OpenAI_Parser

--- src/openai_parser.py
+++ src/openai_parser.py
@@ -40,10 +40,9 @@ class OpenAIParser:
         try:
             response = openai.ChatCompletion.create(model = "gpt-3.5-turbo-0301",
                                                 messages = context_messages)
-            self.update_usage(response["usage"]["total_tokens"], userid)
-            return response["choices"][0]["message"]["content"]
+            return (response["choices"][0]["message"]["content"], response["usage"]["total_tokens"])
         except Exception as e:
-            return str(e) + "\nSorry, I am not feeling well. Please try again."
+            return (str(e) + "\nSorry, I am not feeling well. Please try again.", 0)
 
     def speech_to_text(self, userid, audio_file):
         # transcript = openai.Audio.transcribe("whisper-1", audio_file, language="zh")
@@ -56,55 +55,8 @@ class OpenAIParser:
         # self.update_image_generation_usage(userid)
         # for debug use
         # image_url = "https://catdoctorofmonroe.com/wp-content/uploads/2020/09/iconfinder_cat_tied_275717.png"
-        return image_url
-    
-    def update_image_generation_usage(self, userid):
-        # get time
-        usage_file_name = datetime.datetime.now().strftime("%Y%m") + "_image_generation_usage.json"
-        now = datetime.datetime.now().strftime("%Y-%m-%d")
-        # create usage folder
-        if not os.path.exists("./usage"):
-            os.makedirs("./usage")
-        # load usage or create new
-        if os.path.exists("./usage/" + usage_file_name):
-            with open("./usage/" + usage_file_name) as f:
-                self.usage_dict = json.load(f)
-        else:
-            self.usage_dict = {}
-        # update usage
-        if now not in self.usage_dict:
-            self.usage_dict[now] = {}
-        if userid not in self.usage_dict[now]:
-            self.usage_dict[now][userid] = {"requests": 0}
-        self.usage_dict[now][userid]["requests"] += 1
-        # save usage
-        with open("./usage/" + usage_file_name, "w") as f:
-            json.dump(self.usage_dict, f)
-
-    
-    def update_usage(self, total_tokens, userid):
-        # get time
-        usage_file_name = datetime.datetime.now().strftime("%Y%m") + "_usage.json"
-        now = datetime.datetime.now().strftime("%Y-%m-%d")
-        # create usage folder
-        if not os.path.exists("./usage"):
-            os.makedirs("./usage")
-        # load usage or create new
-        if os.path.exists("./usage/" + usage_file_name):
-            with open("./usage/" + usage_file_name) as f:
-                self.usage_dict = json.load(f)
-        else:
-            self.usage_dict = {}
-        # update usage
-        if now not in self.usage_dict:
-            self.usage_dict[now] = {}
-        if userid not in self.usage_dict[now]:
-            self.usage_dict[now][userid] = {"tokens": 0, "requests": 0}
-        self.usage_dict[now][userid]["tokens"] += total_tokens
-        self.usage_dict[now][userid]["requests"] += 1
-        # save usage
-        with open("./usage/" + usage_file_name, "w") as f:
-            json.dump(self.usage_dict, f)
+        usage = 1 # reserve for future use
+        return (image_url, usage)
 
 if __name__ == "__main__":
     openai_parser = OpenAIParser()
