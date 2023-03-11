81a06256db059c1545f661fcf24bd9c23748dc72
RayCxggg <chexj18@163.com>
2023-03-11 17:02:59 +0800

    finish A update the config.json from beginning

--- bin/config.py
+++ bin/config.py
@@ -35,9 +35,9 @@ class ConfigShell (Cmd):
 
         for i in range(1, 11):
             if i == 4 or i == 8:
-                update_user_list(str(i))
+                update_user_list(str(i), "Config all items")
             else:
-                update_item(str(i))
+                update_item(str(i), "Config all items")
 
     def do_B(self, args):
         print("\nWhich item do you want to config?")
@@ -84,53 +84,64 @@ class ConfigShell (Cmd):
         update_user_list("8")
 
     def do_exit(self, args):
-        print("Exit configuration.")
+        print("Exit configuration. Run the Bot now!")
         return True
 
     def postloop(self) -> None:
         with open("./config.json", "w") as f:
             json.dump(config_dict, f)
-        print("Config.json file is saved.")
+        print("The config.json file is saved.")
         return super().postloop()
 
 
-def update_user_list(index):
+def update_user_list(index, type="Update one item"):
     print("\nHow do you want to config <", item_dict[index], "> list?")
     print("a. add a new user")
     print("b. remove a user")
     print("c. clear the list")
-    print("Input 'a', 'b' or 'c':")
-    data = sys.stdin.readline().strip('\n')
+    print("d. exit")
 
-    if data == 'a':
-        print("Input ID of the new user:")
-        userid = sys.stdin.readline().strip('\n')
-        allowed_users.append(str(userid))
-        print("New user is added to the allowed list.")
-    elif data == 'b':
-        print("Input ID of the user:")
-        userid = sys.stdin.readline().strip('\n')
-        if not allowed_users.count(str(userid)):
-            print("Error. The user is not in the allow list.")
-            return
+    while True:
+        print("Input 'a', 'b', 'c' or 'd':")
+        data = sys.stdin.readline().strip('\n')
+
+        if data == 'a':
+            print("Input ID of the new user:")
+            userid = sys.stdin.readline().strip('\n')
+            allowed_users.append(str(userid))
+            print("New user is added to the allowed list.")
+            break
+        elif data == 'b':
+            print("Input ID of the user:")
+            userid = sys.stdin.readline().strip('\n')
+            if not allowed_users.count(str(userid)):
+                print("*** Error. The user is not in the allowed list.")
+            else:
+                allowed_users.remove(str(userid))
+                print("The user is removed from the allowed list.")
+                break
+        elif data == 'c':
+            allowed_users.clear()
+            print("The allowed list is cleared.\n")
+            break
+        elif data == 'd':
+            break
         else:
-            allowed_users.remove(str(userid))
-            print("The user is removed from the allowed list.")
-    else:
-        allowed_users.clear()
-        print("The allowed list is cleared.")
+            print("*** Invalid input!")
 
-    print("Input (1-10) to update another item or type 'exit' to finsh configuration.\n")
+    if type == "Update one item":
+        print(
+            "\nInput (1-10) to update another item or type 'exit' to finish configuration.\n")
 
 
-def update_item(index):
+def update_item(index, type="Update one item"):
     if index == "3" or index == "5" or index == "7" or index == "10":
         print("Set <", item_dict[index], "> to 'true' or 'false':")
 
         while True:
             data = sys.stdin.readline().strip('\n')
             if not (data == 'true' or data == 'false'):
-                print("Invalid input! please input 'true' or 'false':")
+                print("*** Invalid input! please input 'true' or 'false':")
             else:
                 break
     else:
@@ -139,7 +150,13 @@ def update_item(index):
 
     config_dict[item_dict[index]] = data
     print("<", item_dict[index], "> is updated.\n")
-    print("Input (1-10) to update another item or type 'exit' to finsh configuration.\n")
+
+    if type == "Update one item":
+        print(
+            "\nInput (1-10) to update another item or type 'exit' to finish configuration.\n")
+    if index == "10" and type == "Config all items":
+        config.postloop()
+        config.do_exit()
 
 
 if __name__ == "__main__":
