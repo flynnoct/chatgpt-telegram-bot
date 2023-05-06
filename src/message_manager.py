642e25d84b279346a942ae20f268312712bd1259
wutang <1967724180@qq.com>
2023-05-06 11:01:30 +0000

    add stream in msm

--- src/message_manager.py
+++ src/message_manager.py
@@ -17,30 +17,31 @@ class MessageManager:
         self.__access_manager = access_manager
         self.__userDict = {}
 
-    async def get_response(self, id, user, message, is_voice = False):
+    async def get_response(self, chat_id, user_id, message_id, message, chat_text_first_chunk_callback, chat_text_append_chunks_callback, is_voice = False):
         LoggingManager.debug("Get response for user: %s" % id, "MessageManager")
         t = time.time()
-
-        if id not in self.__userDict:
+        str_id = str(chat_id)
+        str_user = str(user_id)
+        if str_id not in self.__userDict:
             # new user
-            self.__userDict[id] = ChatSession(t)
+            self.__userDict[str_id] = ChatSession(t)
 
-        async with self.__userDict[id].lock:
-            self.__userDict[id].update(t, message, "user")
+        async with self.__userDict[str_id].lock:
+            self.__userDict[str_id].update(t, message, "user")
                 
             if is_voice == True:
-                self.__userDict[id].set_voice()
+                self.__userDict[str_id].set_voice()
             
             # send user info for statistics            
 
             (answer, usage) = await self.__sendMessage(
-                user, self.__userDict[id].messageList)
+                chat_id, user_id, message_id, self.__userDict[str_id].messageList, chat_text_first_chunk_callback, chat_text_append_chunks_callback)
             
             if is_voice == True:
-                self.__userDict[id].unset_voice()
+                self.__userDict[str_id].unset_voice()
             
-            self.__userDict[id].update(t, answer, "assistant")
-            self.__access_manager.update_usage_info(user, usage, "chat")
+            self.__userDict[str_id].update(t, answer, "assistant")
+            self.__access_manager.update_usage_info(str_user, usage, "chat")
           
         return answer
 
@@ -72,23 +73,29 @@ class MessageManager:
 
         return self.__openai_parser.speech_to_text(user, audio_file)
     
-    async def set_system_role(self, id, user, message):
+    async def set_system_role(self, chat_id, user_id, message_id, message, chat_text_first_chunk_callback, chat_text_append_chunks_callback):
         LoggingManager.debug("Set system role for chat: %s" % id, "MessageManager")
         t = time.time()
-        if id not in self.__userDict:
-            self.__userDict[id] = ChatSession(t, message)       
-        self.__userDict[id].set_system_role(t, message)   
-        
-        # send first sentence
-        (answer, usage) = await self.__sendMessage(user, 
-                [{"role": "system", "content": message}, 
-                 {"role": "user", "content":"Say hello to me."}])
-        self.__access_manager.update_usage_info(user, usage, "chat")
+        str_id = str(chat_id)
+        str_user = str(user_id)
+        if str_id not in self.__userDict:
+            self.__userDict[str_id] = ChatSession(t)  
+            
+        async with self.__userDict[str_id].lock:     
+            self.__userDict[str_id].set_system_role(t, message)   
+            
+            # send first sentence
+            (answer, usage) = await self.__sendMessage(chat_id, user_id, message_id, 
+                    [{"role": "system", "content": message}, 
+                    {"role": "user", "content":"Say hello to me."}],
+                    chat_text_first_chunk_callback, chat_text_append_chunks_callback)
+            self.__access_manager.update_usage_info(str_user, usage, "chat")
+
         return answer
         
 
-    async def __sendMessage(self, user, messageList):
-        ans = await self.__openai_parser.get_response(user, messageList)
+    async def __sendMessage(self, chat_id, user_id, message_id, messageList, chat_text_first_chunk_callback, chat_text_append_chunks_callback):
+        ans = await self.__openai_parser.get_response(chat_id, user_id, message_id, messageList, chat_text_first_chunk_callback, chat_text_append_chunks_callback)
         return ans
     
     
