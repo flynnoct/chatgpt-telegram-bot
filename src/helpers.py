d615990cb162eb47931938a861960cd8ae91d91a
flynnoct <me@flynmail.com>
2023-11-15 17:10:44 +0800

    add full support for markdownv2

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,26 @@
 from telegram import helpers as tg_helpers
 
-# TODO: add more escaping
 def escape_markdownv2(text):
-    all_escaped_text = tg_helpers.escape_markdown(text, version=2)
-    # only allowed code for now
-    unescaped_text = all_escaped_text.replace("\`\`\`", "```")
-    return unescaped_text
+    md_str = tg_helpers.escape_markdown(text, version=2)
+    # Replacements for escaped characters which should be unescaped
+    replacements = {
+        # basic markdown
+        r'\*': '*',     # bold or list
+        r'\_': '_',     # italic or underline
+        r'\~': '~',     # strikethrough
+        r'\[': '[',     # link
+        r'\]': ']',     # link
+        r'\(': '(',     # link
+        r'\)': ')',     # link
+        r'\!': '!',     # image
+        r'\`\`\`': '```', # code block
+        r'\`': '`',     # inline code
+        r'\|': '|',     # spoiler
+        # unicode emoji or other future chars can be easy added here
+    }
+    
+    # Process each replacement sequentially
+    for original, replacement in replacements.items():
+        md_str = md_str.replace(original, replacement)
+        
+    return md_str
